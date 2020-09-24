2fcdbf72ddde48fb9756c32a7dee8c1dace22a63
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-09-23 18:20:36 -0700

    completed lab1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -3,9 +3,9 @@
 #include <random>
 #include <chrono>
 #include <numeric>
-#include <functional>
 
 using namespace std;
+using namespace std::chrono;
 
 bool get_line(string& line, const string& output_to_user){
     cout << output_to_user;
@@ -51,7 +51,7 @@ int main() {
             A.push_back(uniform_int_distribution(gen));
 
         sort(A.begin(), A.end());
-        const int TRIALS = 500;
+        const int TRIALS = 100;
         vector<double> lin_search_trials;
         vector<double> bin_search_trials;
         for(int i = 0;i < TRIALS;i++){
@@ -64,7 +64,6 @@ int main() {
             auto end = chrono::steady_clock::now();
             chrono::duration<double> elapsed_seconds = end - start;
             lin_search_trials.push_back(elapsed_seconds.count());
-
             start = chrono::steady_clock::now();
             bin_search(A, key);
             end = chrono::steady_clock::now();
@@ -72,11 +71,11 @@ int main() {
             bin_search_trials.push_back(elapsed_seconds.count());
         }
 
-        double lin_search_avg_runtime = accumulate(lin_search_trials.begin(), lin_search_trials.end(), 0.0) / lin_search_trials.size();
-        double bin_search_avg_runtime = accumulate(bin_search_trials.begin(), bin_search_trials.end(), 0.0) / bin_search_trials.size();
+        double lin_search_avg_runtime = accumulate(lin_search_trials.begin(), lin_search_trials.end(), 0.0) / TRIALS;
+        double bin_search_avg_runtime = accumulate(bin_search_trials.begin(), bin_search_trials.end(), 0.0) / TRIALS;
 
-        cout << "The average runtime for linear search is: " << lin_search_avg_runtime << endl;
-        cout << "The average runtime for binary search is: " << bin_search_avg_runtime << endl;
+        cout << "The average runtime for linear search is: " << lin_search_avg_runtime << " seconds"<< endl;
+        cout << "The average runtime for binary search is: " << bin_search_avg_runtime << " seconds"<< endl;
     }
     // part b
     while(get_line(userinput, "(part b) enter a positive integer n: ")){
@@ -85,7 +84,9 @@ int main() {
         for(int i = 0;i < n;i++)
             A.push_back(uniform_int_distribution(gen));
 
-        int key = 5000;
+        sort(A.begin(), A.end());
+
+        int key = 50000;
         auto start = chrono::steady_clock::now();
         linear_search(A, key);
         auto end = chrono::steady_clock::now();
@@ -98,7 +99,17 @@ int main() {
         elapsed_seconds = end - start;
         double worst_case_binary = elapsed_seconds.count();
 
-        cout << "The worst case running time for linear search for an array of size " << n << " is: " << worst_case_linear << endl;
-        cout << "The worst case running time for binary search for an array of size " << n << " is: " << worst_case_binary << endl;
+        cout << "The worst case running time for linear search for an array of size " << n << " is: " << worst_case_linear << " seconds" << endl;
+        cout << "The worst case running time for binary search for an array of size " << n << " is: " << worst_case_binary << " seconds" << endl;
+
+        double binary_search_single_line = worst_case_binary / log2(n);
+        double linear_search_single_line = worst_case_linear / n;
+
+        cout << "The worst case running time for linear search for n = 10^15 is: " << pow(10, 15) *  binary_search_single_line << " seconds" << endl;
+        cout << "The worst case running time for binary search for n = 10^15 is: " << 15 * log2(10) * binary_search_single_line << " seconds" << endl;
     }
+    /**
+     * we have log_2(n) * c == s where n is the size of the input, c is the time for one single line and s is the number of seconds.
+     * we solve for c to obtain c = s / log_2(n). This gives us the time for single line we use this to estimate the input when n = 10^(15).
+     */
 }
