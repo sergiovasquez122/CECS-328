33900e7d0272c87ccea2cccb5a4ea27468b85da5
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-10-03 11:31:15 -0700

    added userinput to program

--- lab2/find_smallest_positive_number.cpp
+++ lab2/find_smallest_positive_number.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <random>
+#include <functional>
+#include <algorithm>
 
 using namespace std;
+
+bool get_line(string& line){
+    cout << "Enter a positive integer: ";
+    getline(cin, line);
+    return !line.empty();
+}
+
+vector<int> generate_array(int m){
+    random_device r;
+    mt19937 gen(r());
+    uniform_int_distribution<int> uniformIntDistribution(1, m - 1);
+    int result_size = uniformIntDistribution(gen);
+    vector<int> candiates(m);
+    iota(begin(candiates), end(candiates), 0); // generate candidates from [0, m - 1]
+    shuffle(candiates.begin(), candiates.end(), gen); // shuffle the candidates into random order
+    vector<int> result(result_size);
+    for(int i = 0;i < result.size();i++)
+        result[i] = candiates[i];
+    sort(result.begin(), result.end());
+    return result;
+}
 
 /**
  * T(n) = T(n / 2) + O(1)
@@ -18,13 +42,20 @@ int find_smallest_missing_number(const vector<int>& A){
     return lo;
 }
 
+void display_array(const vector<int>& a){
+    cout << "[";
+    for(int e : a)  cout << e << " ";
+    cout <<"]";
+}
+
 int main() {
-    vector<int> a1 = {0, 1, 3, 6, 8, 9};
-    vector<int> a2 = {2, 5, 7, 11};
-    vector<int> a3 = {0, 1, 2, 3, 4};
-    vector<int> a4 = {12};
-    cout << find_smallest_missing_number(a1)<< endl;
-    cout << find_smallest_missing_number(a2) << endl;
-    cout << find_smallest_missing_number(a3) << endl;
-    cout << find_smallest_missing_number(a4) << endl;
+    string userinput;
+    while(get_line(userinput)){
+        int m = stoi(userinput);
+        auto a = generate_array(m);
+        cout << "input: ";
+        display_array(a);
+        cout << ", " << m << endl;
+        cout << "output: " << find_smallest_missing_number(a) << endl;
+    }
 }
