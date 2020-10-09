ca548abe9e0e91026a4c84093cd2c17810d98720
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-10-08 19:16:55 -0700

    added mode_and_repeated_numbers.cpp

---
+++ exam1_code/mode_and_repeated_numbers.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <chrono>
+
+using namespace std;
+
+struct my_pair{
+    vector<int> modes;
+    vector<int> hist;
+};
+
+bool get_line(const string& prompt, string& userinput){
+    cout << prompt;
+    getline(cin, userinput);
+    return !userinput.empty();
+}
+
+// total runtime is O(n)
+vector<int> histogram_generator(const vector<int>& A){
+    vector<int> result(A.size() + 1);
+    // O(n)
+    for(int e : A){
+        ++result[e];
+    }
+    return result;
+}
+
+// the total runtime is O(n)
+my_pair find_mode_and_numbers_repeated_more_than_once(const vector<int>& A){
+    auto hist = histogram_generator(A);
+    vector<int> modes;
+    vector<int> numbers_repeated_more_than_once;
+    int max_count = 0;
+    // O(n)
+    for(int i = 0;i < hist.size();i++){
+        max_count = max(max_count, hist[i]);
+    }
+    // O(n)
+    for(int i = 0;i < hist.size();i++){
+        if(hist[i] == max_count){
+            modes.emplace_back(i);
+        }
+    }
+    return {modes, hist};
+}
+
+void display_arr(const vector<int>& arr){
+    for(int e : arr) cout << e << " ";
+}
+
+int main() {
+    string userinput;
+    mt19937 gen(chrono::system_clock::now().time_since_epoch().count());
+    while (get_line("enter a positive integer n: ", userinput)) {
+        int n = stoi(userinput);
+        uniform_int_distribution<int> uniform_int_distribution(0, n - 1);
+        vector<int> a;
+        for(int i = 0;i < n;i++){
+            a.push_back(uniform_int_distribution(gen));
+        }
+        cout << "a: [";
+        display_arr(a);
+        cout << "]" << endl;
+        auto the_pair = find_mode_and_numbers_repeated_more_than_once(a);
+        cout << "modes:";
+        for (const auto &mode : the_pair.modes) {
+            cout << " " << mode;
+        }
+        cout << endl;
+        for (int i = 0; i < a.size(); i++) {
+            if (the_pair.hist[i] > 1) {
+                cout << i << " appears " << the_pair.hist[i] << " times" << endl;
+            }
+        }
+        cout << endl;
+    }
+}
+
