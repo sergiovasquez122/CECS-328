ec658b5bc4f756adb18ae8f1cb9451f5e0266bb1
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-10-08 20:58:19 -0700

    added part1.cpp

--- EC1/part1.cpp
+++ EC1/part1.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
-int binary_search(const vector<int>& A){
+bool get_line(const string& prompt, string& userinput){
+    cout << prompt;
+    getline(cin, userinput);
+    return !userinput.empty();
+}
+
+int split_point(const vector<int> &A){
     int lo = 0, hi = A.size() - 1;
     int result = -1;
     while(lo <= hi){
@@ -17,4 +24,25 @@ int binary_search(const vector<int>& A){
     }
     return result + 1;
 }
+
+/**
+ * example usage
+ * enter a binary where the first k elements are '0' and the rest of the n - k elements are '1': 0 0 0 1 1
+ * output:  K = 3
+ */
+
+int main(){
+    string userinput;
+    while(get_line("enter a binary where the first k elements are '0' and the rest of the n - k elements are '1': ", userinput)){
+        vector<int> binary_array;
+        stringstream ss(userinput);
+        int value;
+        while(ss >> value){
+            binary_array.push_back(value);
+        }
+        int k = split_point(binary_array);
+        cout << "K = " << k << endl;
+    }
+}
+
 
